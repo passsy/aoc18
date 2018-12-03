d45e9b5f1fbb854ef5c697e05a2c2febe12cf2da
Pascal Welsch <pascal.welsch@gmail.com>
2018-12-03 14:48:31 +0100

    Solve day 3 - part 2

--- 03/day3.py
+++ 03/day3.py
@@ -10,12 +10,14 @@ with open(path.join(path.dirname(__file__), 'input.txt'), 'r', encoding='utf-8')
 
 
 class Rect:
+    id: str
     x: int
     y: int
     width: int
     height: int
 
-    def __init__(self, x: int, y: int, width: int, height: int):
+    def __init__(self, id: str, x: int, y: int, width: int, height: int):
+        self.id = id
         self.x = x
         self.y = y
         self.width = width
@@ -39,9 +41,30 @@ def squared_claimed_multiple_times(claims: List[Rect]) -> int:
     return multiClaims
 
 
+def claimed_once(claims: List[Rect]) -> Rect:
+    fabric = [[0 for x in range(1000)] for y in range(1000)]
+
+    for rect in claims:
+        for y in range(rect.x, rect.x + rect.width):
+            for x in range(rect.y, rect.y + rect.height):
+                fabric[x][y] = fabric[x][y] + 1
+
+    for rect in claims:
+        try:
+            for y in range(rect.x, rect.x + rect.width):
+                for x in range(rect.y, rect.y + rect.height):
+                    if fabric[x][y] > 1:
+                        raise Exception("has overlap")
+            return rect
+        except Exception:
+            rect.height
+
+    raise Exception("no rect found without overlap")
+
+
 def to_rect(s: str) -> Rect:
-    match = re.match(r'.*@ (\d*),(\d*): (\d*)x(\d*)', s)
-    return Rect(int(match.group(1)), int(match.group(2)), int(match.group(3)), int(match.group(4)))
+    match = re.match(r'#(\d*) @ (\d*),(\d*): (\d*)x(\d*)', s)
+    return Rect(match.group(1), int(match.group(2)), int(match.group(3)), int(match.group(4)), int(match.group(5)))
 
 
 if __name__ == '__main__':
@@ -49,3 +72,4 @@ if __name__ == '__main__':
     parsedClaims = [to_rect(claim) for claim in claims]
 
     print(f"multiple claimed fields: {squared_claimed_multiple_times(parsedClaims)}")
+    print(f"claimed once id: {claimed_once(parsedClaims).id}")
